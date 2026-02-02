8a474f6124f625d824b25ed28246e44a2580aaf4
荣耀小灰灰 <2971469964@qq.com>
2026-02-02 12:36:14 +0800

    Add Gist configuration

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // 配置文件 - 请填入你的 GitHub Gist 信息
 const CONFIG = {
     // GitHub Personal Access Token (需要 gist 权限)
-    GITHUB_TOKEN: '你的GitHub_Token',
+    GITHUB_TOKEN: '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',
 
     // Gist ID (创建 Gist 后获取)
-    GIST_ID: '你的Gist_ID',
+    GIST_ID: '6668e26aa33720ecfabc1b7be318a0f6',
 
     // Gist 中存储数据的文件名
     GIST_FILENAME: 'api_keys.json'
